788627cf5c641ea0e1aa79886ebaee39ee3dfa60
PaRang96 <winterreies94@gmail.com>
2024-05-26 22:13:19 +0900

    start coding mob

---
+++ source/Mob.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class Mob : CharacterBody3D
+{
+    [Export]
+    public int MinSpeed { get; set; } = 10;
+
+    [Export]
+    public int MaxSpeed { get; set; } = 18;
+
+    [Export]
+    public VisibleOnScreenNotifier3D visibilityNotifier { get; set; }
+
+    // This function will be called from the main scene
+    public void Initialize(Vector3 startPosition, Vector3 playerPosition)
+    {
+        LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
+
+        RotateY((float)GD.RandRange(-MathF.PI / 4.0f, MathF.PI / 4.0f));
+
+        int randomSpeed = GD.RandRange(MinSpeed, MaxSpeed);
+        Velocity = Vector3.Forward * randomSpeed;
+        Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+
+        visibilityNotifier.ScreenExited += OnVisibilityNotifierScreenExited;
+    }
+
+    private void OnVisibilityNotifierScreenExited()
+    {
+        QueueFree();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        MoveAndSlide();
+    }
+}
